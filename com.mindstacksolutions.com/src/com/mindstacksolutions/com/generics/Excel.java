24d2108c90ff1d8e87548b3653ee33d61be2db24
shobhanksks <shobhanksks@gmail.com>
2019-03-25 18:46:08 +0530

    test

---
+++ com.mindstacksolutions.com/src/com/mindstacksolutions/com/generics/Excel.java
@@ -0,0 +1,46 @@
+package com.mindstacksolutions.com.generics;
+
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class Excel {
+
+	static String filePath = UtilityConstants.EXCEL_PATH;
+
+	public static void writeExcelSheet(String sheetName, int rowNum, int cellNum, String inputData)
+			throws InvalidFormatException, IOException {
+
+		FileInputStream fis = new FileInputStream(filePath);
+		Workbook wb = WorkbookFactory.create(fis);
+		Sheet sheet = wb.getSheet(sheetName);
+		Row row = sheet.getRow(rowNum);
+		Cell cell = row.getCell(cellNum, Row.CREATE_NULL_AS_BLANK);
+		cell.setCellValue(inputData);
+		FileOutputStream fos = new FileOutputStream(filePath);
+		wb.write(fos);
+		fos.close();
+	}
+
+	public static String readExcelSheet(String sheetName, int rowNum, int cellNum)
+			throws InvalidFormatException, IOException {
+		FileInputStream fis;
+		fis = new FileInputStream(filePath);
+		Workbook wb = WorkbookFactory.create(fis);
+		Sheet sheet = wb.getSheet(sheetName);
+		Row row = sheet.getRow(rowNum);
+		Cell cell = row.getCell(cellNum);
+		String cellValue = cell.getStringCellValue();
+
+		return cellValue;
+	}
+}
+//}
---
+++ com.mindstacksolutions.com/src/com/mindstacksolutions/com/generics/UtilityConstants.java
@@ -0,0 +1,21 @@
+package com.mindstacksolutions.com.generics;
+
+public class UtilityConstants {
+
+
+	// chrome property constants
+	public static final String CHROME_KEY = "webdriver.chrome.driver";
+	public static final String CHROME_PATH = "./LibraryFolder/Drivers/chromedriver";
+
+	// application URl
+	public static final String URL = "http://ec2-13-127-9-162.ap-south-1.compute.amazonaws.com:8080/" ;           //"http://i3mdev.dev.ld:8080";
+
+	// system Administrator credentials
+	public static final String ADMIN_UN = "Admin";
+	public static final String ADMIN_PWD = "Admin";
+
+	// excelpath
+	public static final String EXCEL_PATH = "./LibraryFolder/Excel/exceldata.xls";
+
+}
+
